01176702e2a5a8bcdf825fbdf4ea9b9849ded0e6
Arthur Pastel <arthur.pastel@gmail.com>
2023-02-10 11:20:14 +0100

    feat: monitor cache misses

--- src/fibo.ts
+++ src/fibo.ts
@@ -1,13 +1,17 @@
 export const fibonacci = (n: number): number => {
   const cache: Record<number, number> = {};
+  let misses = 0;
   const inner = (n: number): number => {
     if (n <= 1) {
       return n;
     }
     if (cache[n] === undefined) {
       cache[n] = inner(n - 1) + inner(n - 2);
+      misses += 1;
     }
     return cache[n];
   };
-  return inner(n);
+  const value = inner(n);
+  console.debug(`Misses for ${n}: ${misses}`);
+  return value;
 };
